fd03a8333350626e13e3bf53ea655c8a7f5a0698
Vibe Coder <nigel+VibeCoder@stsoftware.com.au>
2026-06-22 20:40:44 +1000

    Milestone: #307 Reduce dashboard padding and decoration to give data more room (#364)

--- docs/sw-register.js
+++ docs/sw-register.js
@@ -18,7 +18,7 @@
   }
 
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js?v=1.0.197")
+    navigator.serviceWorker.register("./sw.js?v=1.0.198")
       .then((registration) => {
         console.log("SW registered: ", registration);
 
